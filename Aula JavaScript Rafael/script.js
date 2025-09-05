e78997a07b76c349dd26c6368730a233707f1e43
LUCAS LERINA RAMOS <lucas_l_ramos@estudante.sesisenai.org.br>
2025-09-04 21:50:28 -0300

    Aula rafael java

---
+++ Aula JavaScript Rafael/script.js
@@ -0,0 +1,15 @@
+// alert("Olá mundo!")
+// alert("oi")
+// alert("tdo bem?")
+// alert("Você vem sempre aqui?")
+
+let idade
+idade = 18
+alert(idade)
+
+idade="dezoito"
+alert(idade)
+
+const amor = "meu primeiro amor"
+alert(amor)
+
