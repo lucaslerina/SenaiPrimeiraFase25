c206c223a37723eb54cb790fd8cc1049f6aecb02
LUCAS LERINA RAMOS <lucas_l_ramos@estudante.sesisenai.org.br>
2025-09-10 19:54:57 -0300

    atualizações

---
+++ Logica_JS/variaveis.js
@@ -0,0 +1,21 @@
+// //var nome = "Lucas" //string
+
+// console.log(nome)
+
+// //variavel com let
+// let nome = "lucas"
+// nome = "lucas"
+
+
+
+const nome = "Lucas" //string
+const idade = 34 //number
+const masculino = true //boolean
+Lucas = "Lucas"
+
+console.log(nome)
+
+//array
+const fruts = ["laranja","maça","Banana"]
+console.log(frutas[0])
+
