94d947f8b31b19adbb9b255a731197fd042f0fc7
LUCAS LERINA RAMOS <lucas_l_ramos@estudante.sesisenai.org.br>
2025-09-10 20:31:59 -0300

    Aula Sq02

---
+++ Aula JavaScript Rafael/sq02/ex01.js
@@ -0,0 +1,13 @@
+let idade
+
+idade = Number(prompt("Digite sua idade:"))
+
+if(idade >= 18){
+    alert("Maior de Idade")
+    alert("Pode sair pro intervalo")
+}else{
+    alert ("Menor de idade")
+    alert ("pode sair pro recreio")
+}
+
+console.log(idade)
