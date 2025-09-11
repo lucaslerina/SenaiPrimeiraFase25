169bd3e3743d8aa616f4a2d5b5e401638e5c835b
LUCAS LERINA RAMOS <lucas_l_ramos@estudante.sesisenai.org.br>
2025-09-10 21:59:45 -0300

    Exercicios

---
+++ Aula JavaScript Rafael/sq02/Exercicio 4/ex04.js
@@ -0,0 +1,10 @@
+let VerificaPar
+VerificaPar = Number(prompt("Digite um numero"))
+
+if (VerificaPar %2==0 ){
+    alert("É par!")
+    console.log ("É Par!")
+}else{
+    alert("É impar!")
+   console.log ("É Impar!")
+}
