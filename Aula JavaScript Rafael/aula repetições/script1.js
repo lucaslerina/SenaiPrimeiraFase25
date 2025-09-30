ed1479cf0d4b0cc7666a3c7c44964cd2da7ba7a3
LUCAS LERINA RAMOS <lucas_l_ramos@estudante.sesisenai.org.br>
2025-09-30 19:11:31 -0300

    repetições

---
+++ Aula JavaScript Rafael/aula repetições/script1.js
@@ -0,0 +1,11 @@
+/*let acertos = Number(prompt("Quantos acertos?"))
+let valorPorQuestao = 10/16
+let nota = acertos * valorPorQuestao
+alert("sua nota: " + nota)*/
+
+let contador = 0
+
+while(contador<5){
+    alert("Oi")
+     contador = contador + 1
+    }
