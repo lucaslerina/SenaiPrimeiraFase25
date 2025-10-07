b8f367c01ddb6569ba4371f7dada6222aa07047b
LUCAS LERINA RAMOS <lucas_l_ramos@estudante.sesisenai.org.br>
2025-10-06 21:40:20 -0300

    exercicios

--- Aula JavaScript Rafael/aula repetições/exercicios/ex5rep.js
+++ Aula JavaScript Rafael/aula repetições/exercicios/ex5rep.js
@@ -1,2 +1,6 @@
 //Altere o programa anterior para fornecer a tabuada do número que o usuário pedir.
+let numero = Number(prompt ("Digite para ver a rabuada referente: "))
 
+for (let i = 1; i <= 10; i++) {
+  console.log( `numero x ${i} = ${ numero * i}`);
+}
